7479e5707aed5311c3f5674303405f45240c02a7
shikuso2 <143647629+shikuso2@users.noreply.github.com>
2024-09-28 14:13:11 -0500

    Thumb y Administrador

--- PHP-MPQ/thumbnail.php
+++ PHP-MPQ/thumbnail.php
@@ -17,28 +17,28 @@ if ($thumbnail_needs_to_be_generated) {
     $mpq_debug = false;
     $mpq = new MPQArchive("../maps/" . $map, $mpq_debug);
 
-    $thumbnail = $mpq->readFile("war3mapMap.blp");
+    $thumbnail = $mpq->readFile("war3mapPreview.tga") or $mpq->readFile("war3mapMap.tga");
     if ($thumbnail) {
-        $blp_thumbnail = $map . ".blp";
-        file_put_contents($blp_thumbnail, $thumbnail);
-	$blp_thumbnail_escaped = escapeshellarg($blp_thumbnail);
-	$command = "../BLPConverter/build/bin/BLPConverter $blp_thumbnail_escaped";
-	$output = [];
+        $tga_thumbnail = $map . ".tga";
+        file_put_contents($tga_thumbnail, $thumbnail);
+        $tga_thumbnail_escaped = escapeshellarg($tga_thumbnail);
+        $preview_escaped = escapeshellarg($preview);
+        $command = "convert $tga_thumbnail $preview_escaped";
+        $output = [];
         $return_var = 0;
         exec($command, $output, $return_var);
-   } else {
-       $thumbnail = $mpq->readFile("war3mapPreview.tga") or $mpq->readFile("war3mapMap.tga");
-       if ($thumbnail) {
-           $tga_thumbnail = $map . ".tga";
-           file_put_contents($tga_thumbnail, $thumbnail);
-           $tga_thumbnail_escaped = escapeshellarg($tga_thumbnail);
-           $preview_escaped = escapeshellarg($preview);
-           $command = "convert $tga_thumbnail $preview_escaped";
-           $output = [];
-           $return_var = 0;
-           exec($command, $output, $return_var);
-       }
-   }
+    }else{
+        $thumbnail = $mpq->readFile("war3mapMap.blp");
+        if ($thumbnail) {
+            $blp_thumbnail = $map . ".blp";
+            file_put_contents($blp_thumbnail, $thumbnail);
+            $blp_thumbnail_escaped = escapeshellarg($blp_thumbnail);
+            $command = "../BLPConverter/build/bin/BLPConverter $blp_thumbnail_escaped";
+            $output = [];
+            $return_var = 0;
+            exec($command, $output, $return_var);
+        }
+    }
 }
 
 header("Content-Type: image/png");

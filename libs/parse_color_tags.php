b1fd400f8ebd397de945a6134852dc52c3124c29
Franco Montenegro <franco@francomontenegro.com.ar>
2024-10-19 14:59:03 -0300

    Make reg expr used for parsing color tags to be case insensitive.

--- libs/parse_color_tags.php
+++ libs/parse_color_tags.php
@@ -2,14 +2,14 @@
 
 function parse_color_tags($text) {
     // Regular expression to match the custom color format
-    $pattern = '/\|cff([0-9a-fA-F]{6})(.*?)\|r/';
+    $pattern = '/\|cff([0-9a-fA-F]{6})(.*?)\|r/i';
     
     // Replace the custom format with a span element with inline CSS color
     $parsed_text = preg_replace_callback($pattern, function($matches) {
         $color_code = $matches[1]; // The hex color code
         $text = $matches[2]; // The text to be colored
         // Return the text wrapped in a span with the corresponding color
-        return '<span style="color: #' . $color_code . '">' . htmlspecialchars($text) . '</span>';
+        return '<span style="color: #' . $color_code . '">' . ($text) . '</span>';
     }, $text);
     
     return $parsed_text;
